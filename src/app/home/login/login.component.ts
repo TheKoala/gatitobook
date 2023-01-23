24a463cee18c21d1828be7198a187942c9367e75
Felipe Araujo de Lima <2981585@CABO.IFPE>
2023-01-23 11:23:46 -0300

    authentication service creation, and form request implemented

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,10 +1,26 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  usuario = '';
+  senha = '';
 
+  constructor(private authService: AutenticacaoService) {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso');
+      },
+      (error) => {
+        alert('Usuário ou senha inválido')
+        console.log(error);
+      }
+    );
+  }
 }
