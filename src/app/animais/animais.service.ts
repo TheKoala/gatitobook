c4368b3b0d7ab0b5af6f2957aa1cd729f1e69462
Felipe Lima <felipelima8556@gmail.com>
2023-02-27 15:19:04 -0300

    like and delete photos

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,18 +1,17 @@
 import { environment } from './../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimaisService {
-  constructor(
-    private httpClient: HttpClient,
-  ) {}
+  constructor(private httpClient: HttpClient) {}
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
     return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
@@ -20,5 +19,22 @@ export class AnimaisService {
 
   buscarPorId(id: number): Observable<Animal> {
     return this.httpClient.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  exluirAnimal(id: number): Observable<Animal> {
+    return this.httpClient.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.httpClient
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
+        map(() => true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED
+            ? of(false)
+            : throwError(() => error);
+        })
+      );
   }
 }
--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -2,19 +2,43 @@ import { AnimaisService } from './../animais.service';
 import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
 import { Animal } from '../animais';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
   templateUrl: './detalhe-animal.component.html',
-  styleUrls: ['./detalhe-animal.component.css']
+  styleUrls: ['./detalhe-animal.component.css'],
 })
 export class DetalheAnimalComponent {
-  animalId !: number;
-  animal$ !: Observable<Animal>
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor (private animaisService: AnimaisService, private activatedRoute: ActivatedRoute) {
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
     this.animalId = activatedRoute.snapshot.params['animalId'];
     this.animal$ = animaisService.buscarPorId(this.animalId);
+  }
+
+  curtir() {
+    this.animaisService.curtir(this.animalId).subscribe({
+      next: (curtida) => {
+        if(curtida){
+          this.animal$ = this.animaisService.buscarPorId(this.animalId)
+        }
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  excluir() {
+    this.animaisService.exluirAnimal(this.animalId).subscribe({
+      next: () => {
+        this.router.navigate(['/animais/']);
+      },
+      error: (error) => console.log(error),
+    });
   }
 }
